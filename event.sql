20bab2c9ca2d905f3361706d97f5e69bc66e956c
jacquelyn-bradshaw <home@bradshaw.local>
2024-01-10 11:18:16 +0000

    event added to orders table

---
+++ event.sql
@@ -0,0 +1,11 @@
+USE customers;
+
+-- Turn ON Event Scheduler
+SET GLOBAL event_scheduler = ON;
+
+/* Create an event that runs once a month to delete orders that
+were placed more than one year ago */
+CREATE EVENT delete_orders
+ON SCHEDULE EVERY 1 MONTH
+DO
+DELETE FROM orders WHERE order_date < NOW() - INTERVAL 1 YEAR;
