f1b404fe279f53460030797b9314324e56b2005a
jacquelyn-bradshaw <home@bradshaw.local>
2024-01-11 21:53:47 +0000

    stored procedure to find all customers in a location

---
+++ stored_procedure.sql
@@ -0,0 +1,24 @@
+USE customers;
+
+/* A stored procedure that finds all the customers based in London
+and returns their first and last names */
+
+DELIMITER //
+
+CREATE PROCEDURE SelectAllCustomers(city varchar(50))
+BEGIN
+  SELECT f_name, l_name
+  FROM customer_details
+  WHERE customer_id IN(
+    SELECT address_customer_id
+    FROM address
+    WHERE city = city
+  );
+  END //
+
+DELIMITER ;
+
+/* Use the stored procedure to find all find all customers
+based in London */
+
+CALL SelectAllCustomers('London');
